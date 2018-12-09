cd69d806b316a841480991ec0ea5846c246c362e
Steven McHenry <stevenmchenry01@gmail.com>
2018-12-08 16:46:47 -0800

    initial backend setup
    graphql and prisma setup.
    first mutations and queries written.
    graphql yoga implementation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,23 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged on
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        // spread all args in to data field from createItem()
+        ...args
+      }
+    }, info)
+
+    return item
+  }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || []
+  //   //create dog
+  //   const newDog = { name: args.name }
+  //   global.dogs.push(newDog)
+  //   return newDog
+  // }
+}
+
+module.exports = Mutations;
